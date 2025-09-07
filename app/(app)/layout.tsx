39c380c152de0a65c68ca9f1dab019697f3f70d0
Maxime Duhamel <max.duh22@gmail.com>
2025-09-07 23:31:13 +0200

    ensure sidebar state is saved in cookies

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,15 +1,19 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
 import Breadcrumb from '@/components/big/breadcrumb';
 import { Sidebar, MobileSidebar } from '@/components/big/sidebar';
+import { cookies } from "next/headers"
 
-export default function Layout({
+export default async function Layout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const cookieStore = await cookies()
+    const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
+
     return (
         <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col justify-between">
-            <SidebarProvider>
+            <SidebarProvider defaultOpen={defaultOpen}>
                 <Sidebar />
                 <main className="flex-1 overflow-y-auto p-0 md:p-4">
                     <MobileSidebar />
